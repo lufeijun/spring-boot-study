b1f3fab561594d3bac890f8d17d51df07fdcd8ed
lufeijun1234 <lufeijun_1234@126.com>
2023-05-22 14:40:03 +0800

    first blood

---
+++ src/main/java/lufeijun/study/springbootdemo/controller/IndexController.java
@@ -0,0 +1,14 @@
+package lufeijun.study.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return  "hello world, 打开新世界";
+	}
+
+}
