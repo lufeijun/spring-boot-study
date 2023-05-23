10ec7b5acd89e2a871779d64d62a2cbca334690a
lufeijun1234 <lufeijun_1234@126.com>
2023-05-23 17:15:27 +0800

    2023-05-23

--- src/main/java/lufeijun/study/springbootdemo/controller/IndexController.java
+++ src/main/java/lufeijun/study/springbootdemo/controller/IndexController.java
@@ -3,6 +3,9 @@ package lufeijun.study.springbootdemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class IndexController {
 
@@ -11,4 +14,22 @@ public class IndexController {
 		return  "hello world, 打开新世界";
 	}
 
+	@RequestMapping("/json")
+	public Result json() {
+		Result result = new Result(0,"success");
+
+		return result;
+	}
+}
+
+
+class Result {
+	public int status;
+	public String message;
+	public String data;
+
+	public Result(int status , String message) {
+		this.status = status;
+		this.message = message;
+	}
 }
