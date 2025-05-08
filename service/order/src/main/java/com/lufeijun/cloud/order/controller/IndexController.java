adcf24e5bcc7fe9b43499a2756f326093ba1606a
lufeijun_1234 <lufeijun_1234@126.com>
2025-05-08 17:53:41 +0800

    Thu May  8 17:53:41 CST 2025

--- service/order/src/main/java/com/lufeijun/cloud/order/controller/IndexController.java
+++ service/order/src/main/java/com/lufeijun/cloud/order/controller/IndexController.java
@@ -1,15 +1,67 @@
 package com.lufeijun.cloud.order.controller;
 
+import ch.qos.logback.core.testUtil.RandomUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api/order")
 public class IndexController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/info")
     public String info(){
         return "order success....";
+    }
+
+
+    @GetMapping("/to/user")
+    public String toUser(){
+        toUser1();
+
+        return "to user....";
+    }
+
+    private void toUser1(){
+        // 1、获取所有实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-user");
+
+        System.out.println("服务个数：" + instances.size());
+
+        // 2、随便拿一个
+        ServiceInstance instance = instances.get( new Random().nextInt(instances.size()) );
+
+        // 3、拼接url
+        String url = instance.getUri() + "/api/user/info";
+
+        ResponseEntity<String> response = restTemplate.exchange(
+            instance.getUri() + "/api/user/info",
+            HttpMethod.GET,
+            null,
+            String.class
+        );
+
+        // 4、处理请求结果
+        if ( response.getStatusCode().is2xxSuccessful() ) {
+            System.out.println(response.getBody());
+        } else {
+            System.out.println("请求失败");
+        }
     }
 }
--- service/user/src/main/java/com/lufeijun/cloud/user/controller/IndexController.java
+++ service/user/src/main/java/com/lufeijun/cloud/user/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.lufeijun.cloud.user.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class IndexController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping("/info")
     public String info(){
-        return "user success....";
+        return "user success...." + serverPort;
     }
 }
